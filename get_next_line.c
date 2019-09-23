45cf824f7542a8fe12dfa583406c9be269045745
Alexa Guillermina <aguiller@ga-l4.21-school.ru>
2019-09-23 20:04:47 +0300

    add

--- get_next_line.c
+++ get_next_line.c
@@ -14,112 +14,52 @@
 #include "libft/libft.h"
 #include <stdio.h>
 
-char	*ft_strdups(const char *s1)
+int makeline(char **line, char **s, int fd, int status)
 {
-	int		i;
-	char	*a;
+	int i;
 
-	i = ft_strlen(s1);
-	a = (char*)malloc(sizeof(char) * i);
-	if (a == NULL)
-		return (NULL);
-	ft_memcpy(a, s1, i);
-	return (a);
-}
-
-char	*ft_strjoins(char const *s1, char const *s2)
-{
-	int		i;
-	int		j;
-	char	*memstr;
-
-	if ((s1 == NULL || s2 == NULL))
-		return (NULL);
 	i = 0;
-	j = 0;
-	memstr = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
-	if (memstr == NULL)
-		return (NULL);
-	ft_strclr(memstr);
-	while (s1[i] != '\0')
-	{
-		memstr[i] = s1[i];
+	while(s[i] != '\n' || s[i] != '\0')
 		i++;
-	}
-	while (s2[j] != '\0')
+	if (s[i] == '\n')
 	{
-		memstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	return (memstr);
-}
-
-int		crstr(char **line, char *bufer, int status)
-{
-	if (!(*line))
-	{
-		if (bufer[status] == '\n' || status == 0)
-		{
-			if (!(*line = ft_strdups(bufer)))
-			return (-1);
-		}
-		else if (!(*line = ft_strdup(bufer)))
-			return (-1);
+		*line = ft_strsub(&s[fd], 0, i);
+		free(&s[fd]);
+		ft_strdel(&s[fd]);
 	}
 	else
 	{
-		if (bufer[status] == '\n' || status == 0)
-		{
-			if (!((*line) = ft_strjoins(ft_strdup(*line),bufer)))
-				return (-1);
-		}
-		else
-		{ 
-			if (!((*line) = ft_strjoin(ft_strdup(*line),bufer)))
-				return (-1);
-		}
+		if (i == BUFF_SIZE)
+			return (get_next_line(fd, line);
+		*line = ft_strsub(&s[fd], 0, i);
+		free(&s[fd]);
+		ft_strdel(&s[fd]);
 	}
 	return (1);
 }
 
-int		buf_check(char *bufer, int status, char **line, int fd)
-{
-	int	i;
-
-	i = 0;
-	while (bufer[i] != '\n')
-		i++;
-		
-	if (bufer[i] == '\n')
-			return (crstr(line, bufer, status));
-		else if	(status <= BUFF_SIZE)
-		{
-			if(crstr(line, bufer, status))
-				return (get_next_line(fd, line));
-			else
-				return (-1);
-		}	
-
-}
-
 int		get_next_line(const int fd, char **line)
 {
-	char	Bufer[BUFF_SIZE];
-	int		status;
+	static char	*s[400];
+	char 		*mem;
+	char		bufer[BUFF_SIZE + 1];
+	int			status;
 
-	if (fd < 0)
+	if (fd < 0 || line == NULL)
 		return (0);
 	status = 0;
-	while ((status = read(fd, Bufer, BUFF_SIZE)))
+	while ((status = read(fd, bufer, BUFF_SIZE) > 0))
 	{
-		printf("%c", Bufer[status - 1]);
-		printf("%d",status);
-		if (status < 0)
-			return (-1);
-		if (status == 0)
-			return (crstr(line, Bufer, status));
-		
+		bufer[status] = '\0'; 
+		if (s[fd] == NULL)
+			s[fd] = ft_strnew(0);
+		mem = ft_strjoin(s[fd], bufer);
+		free(s[fd]);
+		s[fd] = mem;
 	}
-	return (0);
+	if (status < 0)
+		return (-1);
+	if (status == 0 && s[fd] == NULL)
+		return (0);
+	return (makeline(line, s, fd, status));
 }
